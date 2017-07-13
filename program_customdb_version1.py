eb3c33cacabe7ef9432f844999c6c4b9a0fdc202
JerusalemProgramming.com <jerusalemprogramming@gmail.com>
2017-07-13 23:26:41 +0300

    Create program_customdb_version1.py

---
+++ program_customdb_version1.py
@@ -0,0 +1,44 @@
+## IMPORT MODULES
+import datetime
+import pprint
+import mod_OpenFromCSV_Extract2ListOfDictionaries
+import mod_Connect2MongoDBServer
+import mod_Connect2DB
+import mod_InsertData2DB
+import mod_GetAllDataFromDB
+import mod_GetOneDatumFromDB
+
+## DECLARE VARIABLES 
+## DATA OF VARIABLES TO BE EXTRACTED FROM CSV FILE (BELOW)
+
+## BEGIN MAIN PROGRAM        
+if __name__ == "__main__":
+    
+    ## CALL MODULE.FUNCTION - fn_OpenFromCSV_Extract2ListOfDictionaries(); OPEN CSV DATA FILE
+    ListOfDictionariesFromCSV = mod_OpenFromCSV_Extract2ListOfDictionaries.fn_OpenFromCSV_Extract2ListOfDictionaries()
+    print("\n")
+    print("WITHIN PROGRAM:  type(ListOfDictionariesFromCSV) = ", type(ListOfDictionariesFromCSV))
+    
+    ## DECLARE VARIABLES
+    ## DATA OF VARIABLES TO INSERT TO DB
+    Data2Insert2DB = ListOfDictionariesFromCSV
+    
+    ## CALL MODULE.FUNCTION - fn_Connect2MongoDB(); CONNECT TO MONGO DB SERVER WITH CLIENT VARIABLE
+    MongoDBClient = mod_Connect2MongoDBServer.fn_Connect2MongoDBServer()
+    print("\n")
+    print("WITHIN PROGRAM:  type(MongoDBClient) = ", type(MongoDBClient))
+    
+    ## CALL MODULE.FUNCTION - fn_Connect2DB(); pass 1 parameter:  1.) MongoDBClient; CONNECT TO MONGO DB CLIENT WITH DB VARIABLE
+    db = mod_Connect2DB.fn_Connect2DB(MongoDBClient)
+    print("\n")
+    print("WITHIN PROGRAM:  type(db) = ", type(db))
+    
+    ## CALL MODULE.FUNCTION - fn_InsertData2DB(); pass 2 parameters:  1.) db; 2.) Data2Insert2DB - DOES NOT DELETE DB TO START WITH EMPTY MONGO DB!!!
+    DataInserted2DB = mod_InsertData2DB.fn_InsertData2DB(db, Data2Insert2DB)
+    print("\n")
+    print("WITHIN PROGRAM:  type(DataInserted2DB) = ", type(DataInserted2DB))
+   
+    ## CALL MODULE.FUNCTION - fn_GetAllDataFromDB(); pass 1 parameter:  1.) db
+    GetAllDataFromDB = mod_GetAllDataFromDB.fn_GetAllDataFromDB(db)
+    print("\n")
+    print("WITHIN PROGRAM:  type(GetAllDataFromDB) = ", type(GetAllDataFromDB))
